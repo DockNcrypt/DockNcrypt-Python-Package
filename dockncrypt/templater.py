fad33ea95c3ba6c3351f146afeefaeed8973bb5d
Samir Wankhede <samirsw04@gmail.com>
2025-06-21 22:00:42 +0530

    fixed endpoint logic

--- dockncrypt/templater.py
+++ dockncrypt/templater.py
@@ -1,9 +1,9 @@
 from jinja2 import Environment, FileSystemLoader
 import os
 
-def render_templates(domain, email, dst_dir):
+def render_templates(domain, email, endpoint, dst_dir):
     env = Environment(loader=FileSystemLoader(os.path.join(os.path.dirname(__file__), 'templates')))
-    context = {"domain": domain, "email": email}
+    context = {"domain": domain, "email": email, "endpoint": endpoint}
 
     for name in ["docker-compose.yml.j2", "nginx/nginx.conf.j2","nginx-certbot/nginx-certbot.conf.j2"]:
         template = env.get_template(name)
